8dfabcb9ea19a121a6b5dbd3a899fd15147e399e
IanCastlee <castillo321ian@gmail.com>
2025-04-13 22:14:09 +0800

    republish

--- backend/databaseConnection.php
+++ backend/databaseConnection.php
@@ -1,9 +1,24 @@
-<?php
 
-$host = "localhost";
+<!-- $host = "localhost";
 $username = "root";
 $password = '';
 $database = "vetcare";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+} -->
+
+
+
+
+<?php
+
+$host = "sql307.infinityfree.com";
+$username = "if0_38726600";
+$password = 'gKfcV0pCqkQsL';
+$database = "if0_38726600_vetcare";
 
 $conn = mysqli_connect($host, $username, $password, $database);
 
